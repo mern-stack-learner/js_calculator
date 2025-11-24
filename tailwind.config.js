cfb037cfb2d906837189464769b98e24034be76f
dhinababu2025-rebirth <dhinababu2025@gmail.com>
2025-11-24 22:06:23 +0530

    Update UI/UX

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
-	/** @type {import('tailwindcss').Config} */
+// 	/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./*.html", // Crucial: Tells Tailwind to scan all HTML files
+// 	'./src/**/*.{html,js}', // This is the key line 
+// 	//'./index.html', // Add other relevant paths if you use a framework
+//   "./script.js",   // Scan your JS logic
+//   "./style.css",   // Needed because you use @apply here
+// 	  ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html", // Crucial: Tells Tailwind to scan all HTML files
-	'./src/**/*.{html,js}', // This is the key line 
-	'./index.html', // Add other relevant paths if you use a framework
-	  ],
+    "./*.html",  // Crucial: Tells Tailwind to scan all HTML files
+    "./script.js",
+    "./style.css",
+    "./src/**/*.{html,js,css}",  // <-- include input.css here
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
